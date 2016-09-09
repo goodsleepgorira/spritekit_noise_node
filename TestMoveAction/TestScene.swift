1bb7d98908c936ad37a58556cc7a2f8927162071
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-09-09 01:20:58 -0400

    Test

--- TestMoveAction/TestScene.swift
+++ TestMoveAction/TestScene.swift
@@ -6,28 +6,21 @@ import Foundation
 import SpriteKit
 
 class TestScene:SKScene {
-
+    
+    var startDate:NSDate!
+    
     //現在シーン設定時の呼び出しメソッド
     override func didMoveToView(view: SKView) {
         
         //シーンを画面サイズに合わせる。
         self.scaleMode = .AspectFit
         
-        //画面端に物理ボディを設定する。
-        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        
     }
     
     
     //画面タッチ開始時の呼び出しメソッド
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("タッチ開始しました")
+        
+        print("画面をタップしました。")
     }
-
-    //画面タッチ終了時の呼び出しメソッド
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("タッチ終了しました")
-    }
-
-
 }
